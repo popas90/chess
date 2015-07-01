08c17e0432777f4e5d3312064d8482ec74db0f72
popas90 <bogdititupopa@gmail.com>
2015-07-01 22:46:37 +0300

    small add to Board

--- pychess/Board.py
+++ pychess/Board.py
@@ -27,31 +27,35 @@ class Board:
 
         :return:
         """
-        state = self.reset_state()
+        self.reset_board()
 
-    def reset_state(self):
+    def reset_board(self):
         """
 
         :return:
         """
-        board = {}
+        self.board = {}
         for column in Col:
             for row in range(1, 8):
                 location_dict = {"col" : column, "row" : row}
                 if row in (2, 7):
-                    board[column][row] = Pawn(Color.white if row == 2 else Color.black, location_dict, True)
+                    self.board[column][row] = Pawn(Color.white if row == 2 else Color.black, location_dict, True)
                 elif row in (1, 8):
                     if column in (Col.A, Col.H):
-                        board[column][row] = Rook(Color.white if row == 1 else Color.black, location_dict, True)
+                        self.board[column][row] = Rook(Color.white if row == 1 else Color.black, location_dict, True)
                     elif column in (Col.B, Col.G):
-                        board[column][row] = Knight(Color.white if row == 1 else Color.black, location_dict, True)
+                        self.board[column][row] = Knight(Color.white if row == 1 else Color.black, location_dict, True)
                     elif column in (Col.C, Col.F):
-                        board[column][row] = Bishop(Color.white if row == 1 else Color.black, location_dict, True)
+                        self.board[column][row] = Bishop(Color.white if row == 1 else Color.black, location_dict, True)
                     elif column == Col.D:
-                        board[column][row] = Queen(Color.white if row == 1 else Color.black, location_dict, True)
+                        self.board[column][row] = Queen(Color.white if row == 1 else Color.black, location_dict, True)
                     else:
-                        board[column][row] = King(Color.white if row == 1 else Color.black, location_dict, True)
-        return board
+                        self.board[column][row] = King(Color.white if row == 1 else Color.black, location_dict, True)
+
+    def is_empty_location(self, location):
+        if self.board[location["col"]][location["row"]] != None:
+            return False
+        return True
 
     def is_clear_path(self, old_location, new_location):
         """
