c9729eb9ff4c39ab98e116a20fc431c3e9672f73
popas90 <bogdititupopa@gmail.com>
2015-08-02 19:49:42 +0300

    parser work - refactoring

--- pychess/BoardParser.py
+++ pychess/BoardParser.py
@@ -8,12 +8,24 @@ from pychess.King import King
 
 
 class BoardParser:
-    def read_board_from_file(self, file_path):
+    """
+    Used for parsing the text file containing board configurations.
+    """
+
+    @staticmethod
+    def read_board_from_file(file_path):
+        """
+        Parses the persistence file and returns a tuple of board configuration and next-to-move.
+        :param file_path: path to the persistence file
+        :return: board configuration as dict
+        """
         rows, columns = range(1, 10), "ABCDEFGH"
         board = [{col: None for col in columns} for _ in rows]
-
+        next_to_move = None
         try:
             with open(file_path, "r") as file:
+                first_line = file.readline().strip()
+                next_to_move = BoardParser._get_color_from_symbol(first_line)
                 row_iter = iter(rows)
                 for line in file:
                     column_iter = iter(columns)
@@ -21,28 +33,28 @@ class BoardParser:
                     symbols = line.split(" ")
                     for sym in symbols:
                         col = next(column_iter)
-                        board[row][col] = self._get_piece_from_symbol(sym.rstrip(), row, col)
+                        board[row][col] = BoardParser._get_piece_from_token(sym.rstrip(), row, col)
         except ParsingError as err:
             print("An error occurred while parsing the board from file : {0}".format(err))
 
-        return board
+        return next_to_move, board
 
     @staticmethod
-    def _get_piece_from_symbol(sym, row, col):
+    def _get_piece_from_token(token, row, col):
         """
         Returns a new Piece object from the two-letter symbol parameter.
-        :param sym: string representing piece and color
+        :param token: string representing piece and color
         :param row: row location
         :param col: column location
         :return: a new Piece object or None
         """
-        if len(sym) != 2:
-            raise ParsingError("Parsing error: Symbols should contain two characters only: {0}:{1}"
-                               .format(sym, len(sym)))
+        if len(token) != 2:
+            raise ParsingError("Parsing error: Tokens should contain two characters only: {0}:{1}"
+                               .format(token, len(token)))
 
-        color = Color.white if sym[1].capitalize() == "W" else Color.black
-        piece = sym[0].capitalize()
+        color = BoardParser._get_color_from_symbol(token[1])
 
+        piece = token[0].capitalize()
         if piece == "K":
             return King(color, (row, col))
         elif piece == "Q":
@@ -59,6 +71,26 @@ class BoardParser:
             return None
         else:
             raise ParsingError("Parsing error: Unrecognized piece")
+
+    @staticmethod
+    def _get_color_from_symbol(sym):
+        """
+        Returns a Color member, based on the symbol parameter.
+        :param sym: character representing the color
+        :return: a Color member
+        """
+
+        if len(sym) != 1:
+            raise ParsingError("Parsing error: Symbols should contain one character only: {0}:{1}"
+                               .format(sym, len(sym)))
+
+        if sym.capitalize() == "W":
+            color = Color.white
+        elif sym.capitalize() == "B":
+            color = Color.black
+        else:
+            raise ParsingError("Parsing error: Color should be white (W) or black (B)")
+        return color
 
 
 class ParsingError(Exception):
